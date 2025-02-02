8117532b86203cb1232e00d26efb69282b5311c8
naol <naolmeseret22@gmail.com>
2025-02-02 16:56:28 +0300

    testing reports added

--- cypress/e2e/registrationTest.cy.js
+++ cypress/e2e/registrationTest.cy.js
@@ -1,21 +1,76 @@
-import RegistrationPage from '../support/pages/RegistrationPage';
+import RegistrationPage from "../support/pages/RegistrationPage";
+import testData from "../fixtures/testData.json";
 
-describe('Registration Tests', () => {
+describe("Reggestration Test", () => {
   beforeEach(() => {
-    RegistrationPage.visit();
+    RegistrationPage.visit(); // Visit the page before each test
   });
 
-  it('should register a new user successfully', () => {
-    cy.fixture('testData').then((data) => {
-      RegistrationPage.fillForm(data.newUser);
-      RegistrationPage.submit();
-      RegistrationPage.verifyRegistrationMessage('Registration successful');
-    });
+  it("should register a user successfully with valid details", () => {
+    const data = testData.validRegistration;
+
+    RegistrationPage.fillFirstName(data.firstName);
+    RegistrationPage.fillLastName(data.lastName);
+    RegistrationPage.fillEmail(data.email);
+    RegistrationPage.fillReenterEmail(data.reenterEmail);
+    RegistrationPage.fillUsername(data.username);
+    RegistrationPage.fillPassword(data.password);
+    RegistrationPage.getfillBirthdate(data.birthdate);
+    RegistrationPage.selectGender(data.gender);
+    RegistrationPage.confirmTermsAndConditions();
+    RegistrationPage.clickCreateAccount();
+
+    RegistrationPage.assertSuccessMessage(data.successMessage);
   });
 
-  it('should show an error for missing fields', () => {
-    RegistrationPage.fillForm({ username: '', email: '', password: '' });
-    RegistrationPage.submit();
-    RegistrationPage.verifyRegistrationMessage('All fields are required');
+  it("it should show username taken", () => {
+    const data = testData.usernameTaken;
+
+    RegistrationPage.fillFirstName(data.firstName);
+    RegistrationPage.fillLastName(data.lastName);
+    RegistrationPage.fillEmail(data.email);
+    RegistrationPage.fillReenterEmail(data.reenterEmail);
+    RegistrationPage.fillUsername(data.username);
+    RegistrationPage.fillPassword(data.password);
+    RegistrationPage.getfillBirthdate(data.birthdate);
+    RegistrationPage.selectGender(data.gender);
+    RegistrationPage.confirmTermsAndConditions();
+    RegistrationPage.clickCreateAccount();
+
+    RegistrationPage.assertErrorMessage(data.errorMessage);
+  });
+
+  it("should display password too short error", () => {
+    const data = testData.shortPassword;
+
+    RegistrationPage.fillFirstName(data.firstName);
+    RegistrationPage.fillLastName(data.lastName);
+    RegistrationPage.fillEmail(data.email);
+    RegistrationPage.fillReenterEmail(data.reenterEmail);
+    RegistrationPage.fillUsername(data.username);
+    RegistrationPage.fillPassword(data.password);
+    RegistrationPage.getfillBirthdate(data.birthdate);
+    RegistrationPage.selectGender(data.gender);
+    RegistrationPage.confirmTermsAndConditions();
+    RegistrationPage.clickCreateAccount();
+
+    RegistrationPage.assertErrorMessage(data.errorMessage);
+  });
+
+  it("should display terms not agreed error", () => {
+    const data = testData.termsNotAgreed;
+
+    RegistrationPage.fillFirstName(data.firstName);
+    RegistrationPage.fillLastName(data.lastName);
+    RegistrationPage.fillEmail(data.email);
+    RegistrationPage.fillReenterEmail(data.reenterEmail);
+    RegistrationPage.fillUsername(data.username);
+    RegistrationPage.fillPassword(data.password);
+    RegistrationPage.getfillBirthdate(data.birthdate);
+    RegistrationPage.selectGender(data.gender);
+    // Not agreeing to terms
+    RegistrationPage.clickCreateAccount();
+
+    RegistrationPage.assertErrorMessage(data.errorMessage);
   });
 });
--- cypress/support/pages/RegistrationPage.js
+++ cypress/support/pages/RegistrationPage.js
@@ -1,18 +1,70 @@
 class RegistrationPage {
-    visit() {
-      cy.visit('/');
-    }
-  
-    fillForm({ username, email, password }) {
-      cy.get('#username').type(username);
-      cy.get('#email').type(email);
-      cy.get('#password').type(password);
-    }
-  
-    submit() {
-      cy.get('#registerButton').click();
+  visit() {
+    cy.visit("");
+  }
+
+  fillFirstName(firstName) {
+    cy.get(":nth-child(3) > :nth-child(1) > .form-control").type(firstName);
+  }
+
+  fillLastName(lastName) {
+    cy.get(":nth-child(3) > :nth-child(2) > .form-control").type(lastName);
+  }
+
+  fillEmail(email) {
+    cy.get(":nth-child(4) > :nth-child(1) > .form-control").type(email);
+  }
+
+  fillReenterEmail(email) {
+    cy.get(":nth-child(4) > :nth-child(2) > .form-control").type(email);
+  }
+
+  fillUsername(username) {
+    cy.get(":nth-child(5) > .form-control").type(username);
+  }
+
+  fillPassword(password) {
+    cy.get(":nth-child(6) > .form-control").type(password);
+  }
+
+  getfillBirthdate(birthdate) {
+    const [year, month, day] = birthdate.split("-");
+
+    cy.get("input[name='birthdate']")
+      .click()
+      .then(() => {
+        cy.get(".ui-datepicker-year").select(year);
+        cy.get(".ui-datepicker-month").select(month - 1);
+        cy.get(".ui-datepicker-calendar").contains(parseInt(day, 10)).click();
+      });
+  }
+
+  selectGender(gender) {
+    if (gender === "Male") {
+      cy.get(":nth-child(1) > .ossn-radio-input").check({ force: true });
+    } else {
+      cy.get(":nth-child(2) > .ossn-radio-input").check({ force: true });
     }
   }
-  
-  export default new RegistrationPage();
-  
+
+  confirmTermsAndConditions() {
+    cy.get("p > input").click();
+  }
+
+  clickCreateAccount() {
+    cy.get("#ossn-submit-button").click();
+  }
+
+  assertErrorMessage(expectedMessage) {
+    // Increase timeout for this assertion
+    cy.get("#ossn-signup-errors") // 10 seconds timeout
+      .should("contain.text", expectedMessage);
+  }
+
+  assertSuccessMessage(expectedMessage) {
+    // Increase timeout for this assertion
+    cy.get(".ossn-message-done").should("contain.text", expectedMessage);
+  }
+}
+
+export default new RegistrationPage();
